06b23865fafe04e12dd631cc7901c6f20f3ec1e8
Minkyun Kim <mkk0618@naver.com>
2019-10-16 23:32:52 +0900

    BlockHeaderVO class made

---
+++ src/main/java/com/blockchain/biz/BlockHeaderVO.java
@@ -0,0 +1,25 @@
+package com.blockchain.biz;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class BlockHeaderVO {
+	
+	private int height;
+	private String prevBlockHash;
+	private String merkleRoot;
+	private long timeStamp;
+	private String nonce;
+	
+	@Override
+	public String toString() {
+		return "BlockHeaderVO [height: " + height 
+						+ ", prevBlockHash: " + prevBlockHash 
+						+ ", merkleRoot: " + merkleRoot 
+						+ ", timeStamp: " + timeStamp + ", nonce: " + nonce + "]";
+		
+	}
+
+}
